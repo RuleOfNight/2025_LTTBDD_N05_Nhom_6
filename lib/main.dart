6b570637ac57b7c8bc46988480d583c9132ce956
RuleOfNight <s2sakura5s@gmail.com>
2025-10-03 02:03:37 +0700

    feat: initialize main screen and navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:btgk/login/LoginScreen.dart';
+import 'login/LoginScreen.dart';
+import 'screens/main_screen.dart';
 
 void main() {
-  runApp(MaterialApp(home: Material(child: LoginScreen())));
+  runApp(const GamePlatformApp());
+}
+
+class GamePlatformApp extends StatelessWidget {
+  const GamePlatformApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GameHub',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF0F0F1E),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF1A1A2E),
+          elevation: 0,
+        ),
+      ),
+      home: const MainScreen(),
+    );
+  }
 }
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+    final List<Widget> _screens = [
+    const Placeholder(),
+    const Placeholder(),
+    const Placeholder(),
+    const Placeholder(),
+    ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.purple.withOpacity(0.2),
+              blurRadius: 20,
+              offset: const Offset(0, -5),
+            ),
+          ],
+        ),
+        // Navbar
+        child: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: (index) => setState(() => _selectedIndex = index),
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: const Color(0xFF1A1A2E),
+          selectedItemColor: Colors.purpleAccent,
+          unselectedItemColor: Colors.grey,
+          selectedFontSize: 12,
+          unselectedFontSize: 12,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.gamepad), label: 'Games'),
+            BottomNavigationBarItem(icon: Icon(Icons.leaderboard), label: 'Leaderboard'),
+            BottomNavigationBarItem(icon: Icon(Icons.info), label: 'Dev Team'),
+          ],
+        ),
+      ),
+    );
+  }
+}
