1bc541f83737c40f3437f97d1d0d3b0dc272724c
Vương Đào Mạnh <vuong200514@gmail.com>
2025-10-02 22:14:32 +0700

    them khung nhap email

--- lib/login/LoginScreen.dart
+++ lib/login/LoginScreen.dart
@@ -10,8 +10,28 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  var emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // Scaffold là widget cung cấp cấu trúc cơ bản cho một màn hình
+    return Scaffold(
+      // Khi bàn phím bật, Scaffold KHÔNG co lại body
+      resizeToAvoidBottomInset: false,
+      // Có SingleChildScrollView → khi bàn phím bật, người dùng có thể scroll xuống để bấm nút Login
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextFormField(
+              onChanged: (value) => print(value),
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: "Email",
+                hintText: "Enter your email",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
