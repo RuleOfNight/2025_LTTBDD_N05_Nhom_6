f94a44ae9e0099e9afbacf32269ac08d74832b77
Vương Đào Mạnh <vuong200514@gmail.com>
2025-10-05 22:32:15 +0700

    them ham kiem tra chien thang

--- lib/games/tictoc.dart
+++ lib/games/tictoc.dart
@@ -59,6 +59,29 @@ class _TicTocGameState extends State<TicTocGame> {
     });
   }
 
+  void kiemTraNguoiThang() {
+    String nguoiThang = '';
+    List<List<int>> truongHopThang = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (var x in truongHopThang) {
+      if (mangXO[x[0]] != "" &&
+          mangXO[x[0]] == mangXO[x[1]] &&
+          mangXO[x[0]] == mangXO[x[2]]) {
+        nguoiThang = mangXO[x[0]];
+        break;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
