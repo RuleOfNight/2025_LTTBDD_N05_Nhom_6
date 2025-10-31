9aa8dc2a45412821328cd63bdba32693c41294c1
Vương Đào Mạnh <vuong200514@gmail.com>
2025-10-31 16:55:36 +0700

    dua vao ham buildTitile

--- lib/screens/DevTeamScreen.dart
+++ lib/screens/DevTeamScreen.dart
@@ -7,7 +7,6 @@ class DevTeamScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF1A1A2E),
-
       appBar: AppBar(
         title: const Text(
           'Thông tin Nhóm',
@@ -16,34 +15,31 @@ class DevTeamScreen extends StatelessWidget {
         backgroundColor: const Color(0xFF1A1A2E),
         elevation: 0,
       ),
-
+      // 1. Thêm SingleChildScrollView để có thể cuộn
       body: SingleChildScrollView(
+        // 2. Thêm Column để xếp các widget theo chiều dọc
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start, // Căn trái
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-              child: Text(
-                "Thông tin học phần",
-                style: const TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-              child: Text(
-                "Sinh viên thực hiện",
-                style: const TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
+            _buildTitle('Thông tin học phần'),
+
+            _buildTitle('Sinh viên thực hiện'),
+            const SizedBox(height: 20),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
         ),
       ),
     );
